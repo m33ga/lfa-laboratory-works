6a3e8d9fe5ddbe9199184dd6e30dc6e102143488
Mihai Gurduza <mihai.gurduza@isa.utm.md>
2025-05-04 19:22:40 +0300

    update Token class to use TokenType

--- lexer/Token.py
+++ lexer/Token.py
@@ -1,8 +1,10 @@
+from .TokenType import TokenType
+
+
 class Token:
-    def __init__(self, type_, value=None):
+    def __init__(self, type_: TokenType, value=None):
         self.type = type_
         self.value = value
 
     def __repr__(self):
-        return f"Token({self.type}, {repr(self.value)})"
-
+        return f"Token({self.type.name}, {repr(self.value)})"
