5ce834739317e5b9bffd886cc8ebf2b359655e54
Mihai Gurduza <mihai.gurduza@isa.utm.md>
2025-05-04 21:13:53 +0300

    update Token class to keep track of line and column of token

--- lexer/Token.py
+++ lexer/Token.py
@@ -2,9 +2,11 @@ from .TokenType import TokenType
 
 
 class Token:
-    def __init__(self, type_: TokenType, value=None):
+    def __init__(self, type_, value, line, column):
         self.type = type_
         self.value = value
+        self.line = line
+        self.column = column
 
     def __repr__(self):
-        return f"Token({self.type.name}, {repr(self.value)})"
+        return f"Token({self.type}, {repr(self.value)}, line={self.line}, col={self.column})"
